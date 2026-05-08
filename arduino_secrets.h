28004686abb3f69d815160665ed1cfc07bfce35e
havandat1523 <havandat1523@gmail.com>
2026-05-09 04:43:13 +0700

    Update and rename arduino_secrets.h to code/arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_DEVICE_KEY "BUEt2iwwG6nbJE10CS#!glcwT"// nhập mã của thiết bị mình trên đây , lấy từ Arduino IOT cloud
-#define SECRET_OPTIONAL_PASS "" // không sử dụng
-#define SECRET_SSID ""// không sử dụng
---
+++ code/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_DEVICE_KEY "xxxxxxxxxxxxxxxxxxxxx"// nhập mã của thiết bị mình trên đây , lấy từ Arduino IOT cloud
+#define SECRET_OPTIONAL_PASS "" // không sử dụng
+#define SECRET_SSID ""// không sử dụng
